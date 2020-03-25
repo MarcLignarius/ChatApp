0434aaa5fe35ae094568bbebfd66afaa44fef2f7
Marc Davies <marcdaviesriot@gmail.com>
2020-03-25 12:11:18 -0700

    emit messages for welcoming a user, announcing a user has joined, and announcing a user has left

--- server.js
+++ server.js
@@ -10,11 +10,19 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
-// Run when client connects
-io.on("connection", socket => {
-  console.log("New web socket connection...");
+// Runs when client connects
+io.on('connection', socket => {
 
+  // Welcome current user
   socket.emit('message', 'Welcome to Slack That');
+
+  // Broadcast when a user connects
+  socket.broadcast.emit('message', 'A user has joined the chat');
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat');
+  });
 });
 
 const PORT = 3000 || process.env.PORT;
