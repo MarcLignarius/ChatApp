8b5702153b13c8f307e87df699fc0ddc9d7f7b16
Marc Davies <marcdaviesriot@gmail.com>
2020-03-25 12:48:57 -0700

    add formatMessage function

--- server.js
+++ server.js
@@ -1,32 +1,35 @@
-const path = require("path");
-const http = require("http");
-const express = require("express");
-const socketio = require("socket.io");
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 // Set static folder
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const botName = 'SlackDat Bot';
 
 // Runs when client connects
 io.on('connection', socket => {
 
   // Welcome current user
-  socket.emit('message', 'Welcome to SlackDat');
+  socket.emit('message', formatMessage(botName, 'Welcome to SlackDat'));
 
   // Broadcast when a user connects
-  socket.broadcast.emit('message', 'A user has joined the chat');
+  socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
 
   // Runs when client disconnects
   socket.on('disconnect', () => {
-    io.emit('message', 'A user has left the chat');
+    io.emit('message', formatMessage(botName, 'A user has left the chat'));
   });
 
   // Listen for chatMessage
   socket.on('chatMessage', (msg) => {
-      io.emit('message', msg);
+      io.emit('message', formatMessage('USER', msg));
   })
 });
 
