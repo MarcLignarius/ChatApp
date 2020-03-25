4761478c96cdf1da1def0c5eff7a788240fb93d1
Marc Davies <marcdaviesriot@gmail.com>
2020-03-25 13:13:53 -0700

    add userJoin and getCurrentUser functions, display username when user connects

--- server.js
+++ server.js
@@ -1,36 +1,45 @@
-const path = require('path');
-const http = require('http');
-const express = require('express');
-const socketio = require('socket.io');
-const formatMessage = require('./utils/messages');
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const { userJoin, getCurrentUser } = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-const botName = 'SlackDat Bot';
+const botName = "SlackDat Bot";
 
 // Runs when client connects
-io.on('connection', socket => {
+io.on("connection", socket => {
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
 
-  // Welcome current user
-  socket.emit('message', formatMessage(botName, 'Welcome to SlackDat!'));
+    socket.join(user.room);
 
-  // Broadcast when a user connects
-  socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
+    // Welcome current user
+    socket.emit("message", formatMessage(botName, "Welcome to SlackDat!"));
 
-  // Runs when client disconnects
-  socket.on('disconnect', () => {
-    io.emit('message', formatMessage(botName, 'A user has left the chat'));
+    // Broadcast when a user connects
+    socket.broadcast.to(user.room).emit(
+      "message",
+      formatMessage(botName, `${user.username} has joined the chat`)
+    );
   });
 
   // Listen for chatMessage
-  socket.on('chatMessage', (msg) => {
-      io.emit('message', formatMessage('USER', msg));
-  })
+  socket.on("chatMessage", msg => {
+    io.emit("message", formatMessage("USER", msg));
+  });
+
+  // Runs when client disconnects
+  socket.on("disconnect", () => {
+    io.emit("message", formatMessage(botName, `A user has left the chat`));
+  });
 });
 
 const PORT = 3000 || process.env.PORT;
