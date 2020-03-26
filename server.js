d03b047be2ce38a1dd9836d45fc4f0708e2a9208
Marc Davies <marcdaviesriot@gmail.com>
2020-03-26 12:49:47 -0700

    send user list to client

--- server.js
+++ server.js
@@ -36,6 +36,12 @@ io.on("connection", socket => {
         "message",
         formatMessage(botName, `${user.username} has joined the chat`)
       );
+
+    // Send users and room info
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room)
+    });
   });
 
   // Listen for chatMessage
@@ -50,11 +56,20 @@ io.on("connection", socket => {
     const user = userLeave(socket.id);
 
     if (user) {
-      io.to(user.room).emit("message", formatMessage(botName, `${user.username} has left the chat`));
+      io.to(user.room).emit(
+        "message",
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+
+      // Send users and room info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      });
     }
   });
 });
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
