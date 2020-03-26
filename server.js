5aa1975337526dcb6f82ac74b18175633f74d7a5
Marc Davies <marcdaviesriot@gmail.com>
2020-03-26 12:26:07 -0700

    add userLeaver and getRoomUsers functions, send name of user that left to client

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
 const formatMessage = require("./utils/messages");
-const { userJoin, getCurrentUser } = require("./utils/users");
+const {
+  userJoin,
+  userLeave,
+  getCurrentUser,
+  getRoomUsers
+} = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
@@ -25,10 +30,12 @@ io.on("connection", socket => {
     socket.emit("message", formatMessage(botName, "Welcome to SlackDat!"));
 
     // Broadcast when a user connects
-    socket.broadcast.to(user.room).emit(
-      "message",
-      formatMessage(botName, `${user.username} has joined the chat`)
-    );
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
   });
 
   // Listen for chatMessage
@@ -40,7 +47,11 @@ io.on("connection", socket => {
 
   // Runs when client disconnects
   socket.on("disconnect", () => {
-    io.emit("message", formatMessage(botName, `A user has left the chat`));
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("message", formatMessage(botName, `${user.username} has left the chat`));
+    }
   });
 });
 
